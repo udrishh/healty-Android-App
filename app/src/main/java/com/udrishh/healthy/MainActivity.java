f2c92697f767bb23e45f727f2a41211c03c4b1ae
Bogdan Udris <bogdan@Bogdans-MacBook-Pro.local>
2022-03-14 16:42:35 +0200

    Binded the profile fragment

--- app/src/main/java/com/udrishh/healthy/MainActivity.java
+++ app/src/main/java/com/udrishh/healthy/MainActivity.java
@@ -2,6 +2,8 @@ package com.udrishh.healthy;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
@@ -14,14 +16,17 @@ import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
     BottomNavigationView bottomNavigation;
+    Fragment fragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        bottomNavigation = findViewById(R.id.bottom_navigation);
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragment = fragmentManager.findFragmentById(R.id.main_frame_layout);
 
+        bottomNavigation = findViewById(R.id.bottom_navigation);
         bottomNavigation.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
             @SuppressLint("NonConstantResourceId")
             @Override
@@ -29,10 +34,15 @@ public class MainActivity extends AppCompatActivity {
                 switch (item.getItemId()) {
                     case R.id.menu_item_recipes:
                         getSupportActionBar().setTitle("Recipes");
-
                         break;
                     case R.id.menu_item_profile:
                         getSupportActionBar().setTitle("Profile");
+                        if(fragment == null) {
+                            fragment = new ProfileFragment();
+                            fragmentManager.beginTransaction()
+                                    .add(R.id.main_frame_layout, fragment)
+                                    .commit();
+                        }
                         break;
                     case R.id.menu_item_add:
                         getSupportActionBar().setTitle("Add");
