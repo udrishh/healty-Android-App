cbde530a39e9ece81c4940edfe47fd323ffebf4d
Bogdan Udris <bogdan@Bogdans-MacBook-Pro.local>
2022-03-14 18:49:45 +0200

    Created empty recipes fragment

---
+++ app/src/main/java/com/udrishh/healthy/RecipesFragment.java
@@ -0,0 +1,22 @@
+package com.udrishh.healthy;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class RecipesFragment extends Fragment {
+    public RecipesFragment() {
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.fragment_recipes, container, false);
+    }
+}
