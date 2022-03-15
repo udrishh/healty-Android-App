ae26da4338446ab9919a557180e19ed7ca1c9f73
Bogdan Udris <bogdan@Bogdans-MacBook-Pro.local>
2022-03-15 12:27:35 +0200

    Created and binded empty fragments

---
+++ app/src/main/java/com/udrishh/healthy/fragments/AddFragment.java
@@ -0,0 +1,25 @@
+package com.udrishh.healthy.fragments;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.udrishh.healthy.R;
+
+public class AddFragment extends Fragment {
+    public AddFragment() {
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.fragment_add, container, false);
+    }
+}
