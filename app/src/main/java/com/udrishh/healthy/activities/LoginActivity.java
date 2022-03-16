62ed113dd1f3dce9c0ea0dce574bd29fd4dbe4ce
Bogdan Udris <bogdan@Bogdans-MacBook-Pro.local>
2022-03-16 20:38:31 +0200

    Created basic login and signup UI

---
+++ app/src/main/java/com/udrishh/healthy/activities/LoginActivity.java
@@ -0,0 +1,18 @@
+package com.udrishh.healthy.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.udrishh.healthy.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    Butt
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
---
+++ app/src/main/java/com/udrishh/healthy/activities/SignupActivity.java
@@ -0,0 +1,16 @@
+package com.udrishh.healthy.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.udrishh.healthy.R;
+
+public class SignupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+    }
+}
