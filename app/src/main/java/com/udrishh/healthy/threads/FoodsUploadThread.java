2a3bcc2d63036e07dea9f8bf85e5399ffe7d4592
Bogdan Udris <bogdan@Bogdans-MacBook-Pro.local>
2022-03-26 18:34:01 +0200

    Edited Food upload thread

--- app/src/main/java/com/udrishh/healthy/threads/FoodsUploadThread.java
+++ app/src/main/java/com/udrishh/healthy/threads/FoodsUploadThread.java
@@ -2,21 +2,12 @@ package com.udrishh.healthy.threads;
 
 import android.os.Environment;
 import android.util.Log;
-import android.widget.Toast;
 
-import androidx.annotation.NonNull;
-
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.firestore.CollectionReference;
-import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.udrishh.healthy.activities.LoginActivity;
 import com.udrishh.healthy.classes.Food;
 
 import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
@@ -40,45 +31,42 @@ public class FoodsUploadThread extends Thread {
                     new InputStreamReader(connection.getInputStream()));
 
             bufferedReader.readLine();//flush table head
-
             int count = 0;
             while ((line = bufferedReader.readLine()) != null) {
                 count++;
                 Food food = new Food();
                 String[] items = line.split(",");
-
                 food.setName(items[0]);
-                food.setCalories(Float.parseFloat(items[1]));
-                food.setProteins(Float.parseFloat(items[2]));
-                food.setLipids(Float.parseFloat(items[3]));
-                food.setCarbs(Float.parseFloat(items[4]));
-                food.setFibers(Float.parseFloat(items[5]));
+                food.setCalories(Integer.parseInt(items[1]));
+                food.setProteins(Integer.parseInt(items[2]));
+                food.setLipids(Integer.parseInt(items[3]));
+                food.setCarbs(Integer.parseInt(items[4]));
+                food.setFibers(Integer.parseInt(items[5]));
                 food.setUserId(items[6]);
                 food.setFoodId(UUID.randomUUID().toString());
-                Log.d("food_upload", "Read " + count + " out of " + "10814 : SUCCESS");
+                //Log.d("food_upload", "Read " + count + " out of " + "10814 : SUCCESS");
+                Log.d("food_upload", "Read " + count + " out of " + "10814 : " + food.toString());
 
                 //UPLOAD
-                int finalCount = count;
-                collectionReference.add(food)
-                        .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
-                            @Override
-                            public void onSuccess(DocumentReference documentReference) {
-                                Log.d("food_upload", "UPLOAD " + finalCount + " out of " + "10814 SUCCESS");
-                            }
-                        })
-                        .addOnFailureListener(new OnFailureListener() {
-                            @Override
-                            public void onFailure(@NonNull Exception e) {
-                                Log.d("food_upload", "UPLOAD " + finalCount + " out of " + "10814 FAIL");
-                            }
-                        });
+//                int finalCount = count;
+//                collectionReference.add(food)
+//                        .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+//                            @Override
+//                            public void onSuccess(DocumentReference documentReference) {
+//                                Log.d("food_upload", "UPLOAD " + finalCount + " out of " + "10814 SUCCESS");
+//                            }
+//                        })
+//                        .addOnFailureListener(new OnFailureListener() {
+//                            @Override
+//                            public void onFailure(@NonNull Exception e) {
+//                                Log.d("food_upload", "UPLOAD " + finalCount + " out of " + "10814 FAIL");
+//                            }
+//                        });
             }
             Log.d("food_upload", "FINISHED");
             bufferedReader.close();
         } catch (Exception e) {
             Log.w("food_upload", "EXCEPTION OCCURRED : " + e.getMessage());
         }
-
-
     }
 }
