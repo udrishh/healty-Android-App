c44b3b4834452b75f3c4fcaccb7249a5cbd0bf63
Bogdan Udris <bogdan@Bogdans-MacBook-Pro.local>
2022-03-26 17:59:11 +0200

    Created empty fragments

---
+++ app/src/main/java/com/udrishh/healthy/fragments/AddFoodDetailsFragment.java
@@ -0,0 +1,25 @@
+package com.udrishh.healthy.fragments;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.udrishh.healthy.R;
+
+public class AddFoodDetailsFragment extends Fragment {
+    public AddFoodDetailsFragment() {
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.fragment_add_food_details, container, false);
+    }
+}
---
+++ app/src/main/java/com/udrishh/healthy/fragments/AddFoodsSearchFragment.java
@@ -0,0 +1,26 @@
+package com.udrishh.healthy.fragments;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.udrishh.healthy.R;
+
+public class AddFoodsSearchFragment extends Fragment {
+
+    public AddFoodsSearchFragment() {
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.fragment_add_foods_search, container, false);
+    }
+}
