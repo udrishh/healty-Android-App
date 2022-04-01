57c1bf8540f957b30db11ccdcfd5e2c9219913a7
Bogdan Udris <bogdan@Bogdans-MacBook-Pro.local>
2022-04-02 00:04:04 +0300

    Edited Main

--- app/src/main/java/com/udrishh/healthy/activities/MainActivity.java
+++ app/src/main/java/com/udrishh/healthy/activities/MainActivity.java
@@ -1,6 +1,8 @@
 package com.udrishh.healthy.activities;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.annotation.UiThread;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
@@ -8,23 +10,47 @@ import androidx.fragment.app.FragmentManager;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.google.firebase.storage.FileDownloadTask;
+import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.udrishh.healthy.R;
+import com.udrishh.healthy.classes.Food;
+import com.udrishh.healthy.classes.FoodDrinkRecord;
 import com.udrishh.healthy.classes.User;
+import com.udrishh.healthy.enums.RecordType;
+import com.udrishh.healthy.enums.Sex;
 import com.udrishh.healthy.fragments.SettingsFragment;
 import com.udrishh.healthy.fragments.AddFragment;
 import com.udrishh.healthy.fragments.ProfileFragment;
 import com.udrishh.healthy.fragments.RecipesFragment;
 import com.udrishh.healthy.fragments.StatisticsFragment;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
+import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
     private BottomNavigationView bottomNavigation;
@@ -32,14 +58,49 @@ public class MainActivity extends AppCompatActivity {
     private FragmentManager fragmentManager;
 
     private User user;
+    private ArrayList<Food> foods = new ArrayList<>();
+    private ArrayList<FoodDrinkRecord> foodDrinkRecords = new ArrayList<>();
 
     private FirebaseAuth firebaseAuth;
     private FirebaseAuth.AuthStateListener authStateListener;
     private FirebaseFirestore db = FirebaseFirestore.getInstance();
     private StorageReference storageReference;
 
+    private CollectionReference foodDrinkRecordsReference = db.collection("Records");
+
+    public void addFoodDrinkRecord(FoodDrinkRecord foodDrinkRecord) {
+        if (foodDrinkRecord != null) {
+            foodDrinkRecords.add(foodDrinkRecord);
+            Log.d("mytag", foodDrinkRecord.toString());
+            Log.d("mytag", "Record added succesfully");
+
+            //add to firebase
+            foodDrinkRecordsReference.add(foodDrinkRecord)
+                    .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                        @Override
+                        public void onSuccess(DocumentReference documentReference) {
+                            Log.d("mytag", "Record was added to firebase successfully!");
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Log.d("mytag", "Error occurred while adding record to firebase!");
+                        }
+                    });
+        }
+    }
+
     public User getUserObject() {
         return user;
+    }
+
+    public ArrayList<Food> getFoods() {
+        return foods;
+    }
+
+    public ArrayList<FoodDrinkRecord> getFoodDrinkRecords() {
+        return foodDrinkRecords;
     }
 
     public FirebaseAuth getFirebaseAuth() {
@@ -61,11 +122,21 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onStart() {
         super.onStart();
+
+        Log.d("mytag","MainActivity onStart");
+        loadDatabase();
+        //load records
+        if(foodDrinkRecords.isEmpty()){
+            loadUserFoodDrinkRecords();
+        }
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        Log.d("mytag","MainActivity onCreate");
+
         setContentView(R.layout.activity_main);
 
         Intent intent = getIntent();
@@ -119,6 +190,121 @@ public class MainActivity extends AppCompatActivity {
                 return true;
             }
         });
+
+    }
+
+    private void loadDatabase() {
+        Log.d("mytag", "Database task started!");
+        new Thread() {
+            @Override
+            public void run() {
+                super.run();
+
+                StorageReference storageReference = FirebaseStorage.getInstance().getReference();
+                StorageReference filepath = storageReference.child("foods_db.csv");
+
+                File localFile = null;
+                try {
+                    localFile = File.createTempFile("foods_db", "csv");
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                File finalLocalFile = localFile;
+                Log.d("mytag", finalLocalFile.toString());
+                filepath.getFile(localFile).addOnSuccessListener(new OnSuccessListener<FileDownloadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(FileDownloadTask.TaskSnapshot taskSnapshot) {
+                        try {
+                            Log.d("mytag", "Started loading database!");
+                            Log.d("mytag", finalLocalFile.toString());
+                            BufferedReader bufferedReader = new BufferedReader(new FileReader(finalLocalFile));
+                            bufferedReader.readLine();
+                            String line;
+                            while ((line = bufferedReader.readLine()) != null) {
+                                String[] items = line.split(",");
+                                Food food = new Food();
+                                food.setName(items[0]);
+                                food.setCalories(Integer.parseInt(items[1]));
+                                food.setProteins(Integer.parseInt(items[2]));
+                                food.setLipids(Integer.parseInt(items[3]));
+                                food.setCarbs(Integer.parseInt(items[4]));
+                                food.setFibers(Integer.parseInt(items[5]));
+                                food.setUserId(items[6]);
+                                food.setFoodId(UUID.randomUUID().toString());
+
+                                foods.add(food);
+                                //Log.d("mytag", food.toString());
+                            }
+                            bufferedReader.close();
+                            Log.d("mytag", "Successfully loaded database!");
+                        } catch (Exception e) {
+                            Log.d("mytag", "Exception occured: " + e.getMessage());
+                        }
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception exception) {
+                        Log.d("mytag", "Exception occured: " + exception.getMessage());
+                    }
+                }).addOnCompleteListener(new OnCompleteListener<FileDownloadTask.TaskSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<FileDownloadTask.TaskSnapshot> task) {
+                        if (task.isSuccessful()) {
+                            //DATABASE LOADED SUCCESSFULLY
+                        }
+                    }
+                });
+
+                Log.d("mytag", "Database task finished!");
+            }
+        }.start();
+    }
+
+    private void loadUserFoodDrinkRecords() {
+        Log.d("mytag","MainActivity loading records............");
+
+        foodDrinkRecordsReference
+                .whereEqualTo("userId", user.getUserId())
+                .addSnapshotListener(new EventListener<QuerySnapshot>() {
+                    @Override
+                    public void onEvent(@Nullable QuerySnapshot queryDocumentSnapshots,
+                                        @Nullable FirebaseFirestoreException e) {
+                        if (e != null) {
+                            return;
+                        }
+
+                        if (!queryDocumentSnapshots.isEmpty() && foodDrinkRecords.isEmpty()) {
+                            for (QueryDocumentSnapshot snapshot : queryDocumentSnapshots) {
+                                FoodDrinkRecord foodDrinkRecord = new FoodDrinkRecord();
+                                foodDrinkRecord.setRecordId(snapshot.getString("recordId"));
+                                foodDrinkRecord.setUserId(snapshot.getString("userId"));
+                                foodDrinkRecord.setItemId(snapshot.getString("itemId"));
+                                foodDrinkRecord.setDate(snapshot.getString("date"));
+                                foodDrinkRecord.setName(snapshot.getString("name"));
+                                foodDrinkRecord.setProteins(snapshot.get("proteins", Integer.class));
+                                foodDrinkRecord.setLipids(snapshot.get("lipids", Integer.class));
+                                foodDrinkRecord.setCarbs(snapshot.get("carbs", Integer.class));
+                                foodDrinkRecord.setFibers(snapshot.get("fibers", Integer.class));
+                                foodDrinkRecord.setQuantity(snapshot.get("quantity", Integer.class));
+                                foodDrinkRecord.setTotalCalories(snapshot.get("totalCalories", Integer.class));
+                                foodDrinkRecord.setTotalProteins(snapshot.get("totalProteins", Integer.class));
+                                foodDrinkRecord.setTotalFibers(snapshot.get("totalFibers", Integer.class));
+                                foodDrinkRecord.setTotalCarbs(snapshot.get("totalCarbs", Integer.class));
+                                foodDrinkRecord.setTotalLipids(snapshot.get("totalLipids", Integer.class));
+                                if (snapshot.getString("category").equals("FOOD")) {
+                                    foodDrinkRecord.setCategory(RecordType.FOOD);
+                                } else {
+                                    foodDrinkRecord.setCategory(RecordType.DRINK);
+                                }
+
+                                foodDrinkRecords.add(foodDrinkRecord);
+                                Log.d("mytag", "Record was retrieved from firebase successfully!");
+                            }
+                            setProfileFragment();
+                        }
+                    }
+                });
 
     }
 }
