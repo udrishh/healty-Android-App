828592c718373a16784c21cda43f0cb6a28299d2
Bogdan Udris <bogdan@Bogdans-MacBook-Pro.local>
2022-04-07 16:28:35 +0300

    Created Drink Adapter

--- app/src/main/java/com/udrishh/healthy/adapters/DrinksAdapter.java
+++ app/src/main/java/com/udrishh/healthy/adapters/DrinksAdapter.java
@@ -4,32 +4,60 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.BaseAdapter;
+import android.widget.Filter;
 import android.widget.TextView;
+
+import androidx.annotation.NonNull;
 
 import com.udrishh.healthy.R;
 import com.udrishh.healthy.classes.Drink;
 
 import java.util.ArrayList;
+import java.util.List;
 
-public class DrinksAdapter extends BaseAdapter {
+public class DrinksAdapter extends ArrayAdapter<Drink> {
     Context context;
     ArrayList<Drink> drinks;
+    ArrayList<Drink> filteredDrinksList;
 
-    public DrinksAdapter(Context context, ArrayList<Drink> drinks) {
-        this.context = context;
-        this.drinks = drinks;
+    public DrinksAdapter(@NonNull Context context, int resource, @NonNull List<Drink> objects) {
+        super(context, resource, objects);
+        drinks = new ArrayList<>(objects);
     }
 
-    @Override
-    public int getCount() {
-        return drinks.size();
-    }
+    private Filter drinksFilter = new Filter() {
+        @Override
+        protected FilterResults performFiltering(CharSequence constraint) {
+            FilterResults results = new FilterResults();
 
-    @Override
-    public Object getItem(int position) {
-        return drinks.get(position);
-    }
+            filteredDrinksList = new ArrayList<>();
+
+            if (constraint == null || constraint.length() == 0) {
+                filteredDrinksList.addAll(drinks);
+            } else {
+                String filterPattern = constraint.toString().toLowerCase().trim();
+                for (Drink drink : drinks) {
+                    if (drink.getName().toLowerCase().contains(filterPattern)) {
+                        filteredDrinksList.add(drink);
+                    }
+                }
+            }
+
+            results.values = filteredDrinksList;
+            results.count = filteredDrinksList.size();
+
+            return results;
+        }
+
+        @Override
+        protected void publishResults(CharSequence constraint, FilterResults results) {
+            clear();
+            addAll((List) results.values);
+            notifyDataSetChanged();
+        }
+    };
 
     @Override
     public long getItemId(int position) {
@@ -38,16 +66,27 @@ public class DrinksAdapter extends BaseAdapter {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        if(convertView == null) {
-            convertView = LayoutInflater.from(context).inflate(R.layout.food_drink_item, parent, false);
+        if (convertView == null) {
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.food_drink_item, parent, false);
         }
 
-        TextView drinkName;
-        TextView drinkCalories;
+        TextView drinkName = convertView.findViewById(R.id.food_drink_item_name);
+        TextView drinkCalories = convertView.findViewById(R.id.food_drink_item_calories);
 
-        drinkName = convertView.findViewById(R.id.food_drink_item_name);
-        drinkCalories = convertView.findViewById(R.id.food_drink_item_calories);
+        Drink drink = getItem(position);
+        if(drink!=null){
+            drinkName.setText(drink.getName());
+            drinkCalories.setText(String.valueOf(drink.getCalories())+"\nkcal/100g");
+        }
 
         return convertView;
     }
+
+    @NonNull
+    @Override
+    public Filter getFilter() {
+        return drinksFilter;
+    }
+
+
 }
