3f868fdac93fe5aad87bb3675f62e10085c8f25b
Bogdan Udris <bogdan@Bogdans-MacBook-Pro.local>
2022-04-08 14:52:39 +0300

    Created Physical Activity Record class

---
+++ app/src/main/java/com/udrishh/healthy/classes/PhysicalActivityRecord.java
@@ -0,0 +1,81 @@
+package com.udrishh.healthy.classes;
+
+import com.udrishh.healthy.enums.RecordType;
+
+public class PhysicalActivityRecord {
+    private String recordId;
+    private String userId;
+    private String itemId;
+    private String name;
+    private int calories;
+    private int duration;
+    private int totalCalories;
+    private String Date;
+
+    public PhysicalActivityRecord() {
+    }
+
+    public String getRecordId() {
+        return recordId;
+    }
+
+    public void setRecordId(String recordId) {
+        this.recordId = recordId;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getItemId() {
+        return itemId;
+    }
+
+    public void setItemId(String itemId) {
+        this.itemId = itemId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getCalories() {
+        return calories;
+    }
+
+    public void setCalories(int calories) {
+        this.calories = calories;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    public int getTotalCalories() {
+        return totalCalories;
+    }
+
+    public void setTotalCalories(int totalCalories) {
+        this.totalCalories = totalCalories;
+    }
+
+    public String getDate() {
+        return Date;
+    }
+
+    public void setDate(String date) {
+        Date = date;
+    }
+}
