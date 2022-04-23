6dca871d65e8414292131fd01c53b9ee1cb4e5a4
Bogdan Udris <bogdan@Bogdans-MacBook-Pro.local>
2022-04-23 11:58:20 +0300

    Added History in Statistics

---
+++ app/src/main/java/com/udrishh/healthy/adapters/RecordAdapter.java
@@ -0,0 +1,95 @@
+package com.udrishh.healthy.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+import com.udrishh.healthy.R;
+import com.udrishh.healthy.classes.FoodDrinkRecord;
+import com.udrishh.healthy.classes.MeasurementRecord;
+import com.udrishh.healthy.classes.PhysicalActivity;
+import com.udrishh.healthy.classes.PhysicalActivityRecord;
+import com.udrishh.healthy.classes.Recipe;
+import com.udrishh.healthy.classes.RecipeRecord;
+import com.udrishh.healthy.classes.Record;
+import com.udrishh.healthy.enums.RecipeCategory;
+import com.udrishh.healthy.enums.RecordType;
+
+import java.util.ArrayList;
+
+public class RecordAdapter extends BaseAdapter {
+    private Context context;
+    private ArrayList<Record> records;
+
+    public RecordAdapter(Context context, ArrayList<Record> records) {
+        this.context = context;
+        this.records = records;
+    }
+
+    @Override
+    public int getCount() {
+        return records.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return records.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        if (convertView == null) {
+            convertView = LayoutInflater.from(context).inflate(R.layout.record_item, parent, false);
+        }
+
+        TextView name = convertView.findViewById(R.id.record_item_name);
+        TextView date = convertView.findViewById(R.id.record_item_date);
+        ImageView icon = convertView.findViewById(R.id.record_item_icon);
+
+        if (records.get(position) instanceof FoodDrinkRecord) {
+            //FOOD OR DRINK RECORD
+            FoodDrinkRecord foodDrinkRecord = (FoodDrinkRecord) records.get(position);
+            if (foodDrinkRecord.getCategory() == RecordType.DRINK) {
+                icon.setImageResource(R.drawable.glass_icon);
+            } else {
+                icon.setImageResource(R.drawable.food_icon);
+            }
+            name.setText(context.getString(R.string.record_item_name, foodDrinkRecord.getName()));
+            String[] dates = foodDrinkRecord.getDate().split(" - ");
+            date.setText(context.getString(R.string.record_item_date, dates[0], dates[1]));
+        } else if (records.get(position) instanceof PhysicalActivityRecord) {
+            //PHYSICAL ACTIVITY RECORD
+            PhysicalActivityRecord physicalActivityRecord = (PhysicalActivityRecord) records.get(position);
+            icon.setImageResource(R.drawable.activity_icon);
+            name.setText(context.getString(R.string.record_item_name, physicalActivityRecord.getName()));
+            String[] dates = physicalActivityRecord.getDate().split(" - ");
+            date.setText(context.getString(R.string.record_item_date, dates[0], dates[1]));
+        } else if (records.get(position) instanceof MeasurementRecord) {
+            //MEASUREMENT RECORD
+            MeasurementRecord measurementRecord = (MeasurementRecord) records.get(position);
+            icon.setImageResource(R.drawable.measure_icon);
+            name.setText(context.getString(R.string.record_item_name, measurementRecord.getName()));
+            String[] dates = measurementRecord.getDate().split(" - ");
+            date.setText(context.getString(R.string.record_item_date, dates[0], dates[1]));
+        } else if (records.get(position) instanceof RecipeRecord) {
+            //RECIPE RECORD
+            RecipeRecord recipeRecord = (RecipeRecord) records.get(position);
+            icon.setImageResource(R.drawable.meal_icon);
+            name.setText(context.getString(R.string.record_item_name, recipeRecord.getName()));
+            String[] dates = recipeRecord.getDate().split(" - ");
+            date.setText(context.getString(R.string.record_item_date, dates[0], dates[1]));
+        }
+
+        return convertView;
+    }
+}
--- app/src/main/java/com/udrishh/healthy/fragments/StatisticsFragment.java
+++ app/src/main/java/com/udrishh/healthy/fragments/StatisticsFragment.java
@@ -9,12 +9,23 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import com.udrishh.healthy.R;
+import com.udrishh.healthy.activities.MainActivity;
+import com.udrishh.healthy.adapters.RecipeItemAdapter;
+import com.udrishh.healthy.adapters.RecordAdapter;
+import com.udrishh.healthy.classes.Record;
+
+import java.util.ArrayList;
 
 public class StatisticsFragment extends Fragment {
     private View view;
-    
+    private TextView recordsTitle;
+    private ListView recordsList;
+    private ArrayList<Record> records;
+
     public StatisticsFragment() {
     }
 
@@ -22,10 +33,34 @@ public class StatisticsFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_statistics, container, false);
+        getRecords();
         initialiseComponents();
         return view;
     }
 
+    private void getRecords() {
+        records = new ArrayList<>();
+        records = (((MainActivity) this.requireActivity()).getRecords());
+    }
+
     private void initialiseComponents() {
+        recordsTitle = view.findViewById(R.id.statistics_record_card_title);
+        recordsList = view.findViewById(R.id.statistics_record_list);
+
+        if (records.isEmpty()) {
+            recordsTitle.setText(getString(R.string.statistics_no_records_text));
+        } else {
+            ArrayList<Record> lastRecords = new ArrayList<>();
+            int nb = Math.min(records.size(), 5);
+            if(nb == 1){
+                recordsTitle.setText(getString(R.string.statistics_one_stats_text, nb));
+            } else {
+                recordsTitle.setText(getString(R.string.statistics_stats_text, nb));
+            }
+            for (int i = 1; i <= nb; i++) {
+                lastRecords.add(records.get(records.size() - i));
+            }
+            recordsList.setAdapter(new RecordAdapter(getContext(), lastRecords));
+        }
     }
 }
